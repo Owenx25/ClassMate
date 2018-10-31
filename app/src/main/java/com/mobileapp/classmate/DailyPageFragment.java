3382a8aa0d0cd11ce0d6e69fbe1512aa6fb9dc6d
Owen McCormack <Owen_McCormack@student.uml.edu>
2018-10-31 16:48:31 -0400

    very basic ViewPager

---
+++ app/src/main/java/com/mobileapp/classmate/DailyPageFragment.java
@@ -0,0 +1,18 @@
+package com.mobileapp.classmate;
+
+import android.support.v4.app.Fragment;
+import android.view.View;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.os.Bundle;
+
+public class DailyPageFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(
+                R.layout.daily_layout, container, false);
+        return rootView;
+    }
+}
