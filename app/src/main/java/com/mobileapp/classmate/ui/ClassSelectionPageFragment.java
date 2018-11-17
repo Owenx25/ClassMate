4b40a912cd02f98fc1a45ef377d7f811299430c9
Owen McCormack <Owen_McCormack@student.uml.edu>
2018-11-17 10:51:30 -0500

    DB finally working for adding classes, landscape mode tab fix

--- app/src/main/java/com/mobileapp/classmate/ui/ClassSelectionPageFragment.java
+++ app/src/main/java/com/mobileapp/classmate/ui/ClassSelectionPageFragment.java
@@ -1,8 +1,13 @@
 package com.mobileapp.classmate.ui;
 
+import android.app.AlertDialog;
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.annotation.ColorInt;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -11,12 +16,18 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.mobileapp.classmate.R;
 import com.mobileapp.classmate.db.entity.Course;
 import com.mobileapp.classmate.viewmodel.MainViewModel;
+import com.pes.androidmaterialcolorpickerdialog.ColorPicker;
+import com.pes.androidmaterialcolorpickerdialog.ColorPickerCallback;
 
+import java.util.Date;
 import java.util.List;
 
 public class ClassSelectionPageFragment extends Fragment {
@@ -49,5 +60,64 @@ public class ClassSelectionPageFragment extends Fragment {
         viewModel.getAllCourses().observe(this, courses -> adapter.setCourses(courses));
 
         return rootView;
+    }
+
+    public void showAddCourseDialog() {
+        LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+        View AddCourseView = layoutInflater.inflate(R.layout.dialog_addcourse, null);
+        final AlertDialog alertD = new AlertDialog.Builder(getActivity())
+                .setTitle(R.string.add_course_dialog)
+                .create();
+        EditText courseInput = (EditText) AddCourseView.findViewById(R.id.course_name);
+        Button colorBtn = (Button) AddCourseView.findViewById(R.id.button_color);
+        Button saveBtn = (Button) AddCourseView.findViewById(R.id.button_addcourse_save);
+        Button cancelBtn = (Button) AddCourseView.findViewById(R.id.button_addcourse_cancel);
+        // OnClick Callbacks
+        final ColorPicker cp = new ColorPicker(getActivity(), 0,0,0);
+        cp.enableAutoClose();
+
+        alertD.setView(AddCourseView);
+        alertD.show();
+
+        // Show Course add dialog after color is picked
+        cp.setCallback(new ColorPickerCallback() {
+            @Override
+            public void onColorChosen(@ColorInt int color) {
+                alertD.show();
+                View colorSquare = alertD.findViewById(R.id.color_square);
+                colorSquare.setBackgroundColor(color);
+            }
+        });
+
+        // Show Color picker on color button press
+        colorBtn.setOnClickListener(v -> {
+            alertD.hide();
+            cp.show();
+        });
+
+        // Validate Course inputs and add to DB
+        saveBtn.setOnClickListener(v -> {
+            // Don't let user save if
+            //  - Course is empty
+            //  - Course already exists(NOT YET IMPLEMENTED)
+            int color = Color.TRANSPARENT;
+            View colorSquare = alertD.findViewById(R.id.color_square);
+            Drawable background = colorSquare.getBackground();
+            if (background instanceof ColorDrawable)
+                color = ((ColorDrawable)background).getColor();
+
+            if (courseInput.getText().toString().matches("")) {
+                Toast.makeText(getActivity(), R.string.invalid_course, Toast.LENGTH_SHORT).show();
+            } else {
+                viewModel.insertCourse(new Course(
+                        courseInput.getText().toString(),
+                        new Date(),
+                        color));
+                alertD.dismiss();
+            }
+        });
+
+        // Quit on cancel press
+        cancelBtn.setOnClickListener(v -> alertD.dismiss());
     }
 }
--- app/src/main/java/com/mobileapp/classmate/ui/ScreenSliderPagerActivity.java
+++ app/src/main/java/com/mobileapp/classmate/ui/ScreenSliderPagerActivity.java
@@ -1,29 +1,13 @@
 package com.mobileapp.classmate.ui;
 
-import android.app.AlertDialog;
-import android.arch.lifecycle.ViewModelProviders;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.support.annotation.ColorInt;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
 import com.mobileapp.classmate.R;
-import com.mobileapp.classmate.db.entity.Course;
 import com.mobileapp.classmate.viewmodel.MainViewModel;
-import com.pes.androidmaterialcolorpickerdialog.ColorPicker;
-import com.pes.androidmaterialcolorpickerdialog.ColorPickerCallback;
-
-import java.util.Date;
 
 // Need to implement tab layout for tabs
 public class ScreenSliderPagerActivity extends FragmentActivity {
@@ -57,8 +41,7 @@ public class ScreenSliderPagerActivity extends FragmentActivity {
         viewPager.setAdapter(mTabAdapter);
         tablayout.setupWithViewPager(viewPager);
 
-
-
+        // Add Class Floating action button
         mFab = (FloatingActionButton) findViewById(R.id.fab);
         viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
@@ -77,66 +60,7 @@ public class ScreenSliderPagerActivity extends FragmentActivity {
             @Override
             public void onPageScrollStateChanged(int state) { }
         });
-        mFab.setOnClickListener(v -> showAddCourseDialog());
-    }
-
-    public void showAddCourseDialog() {
-        LayoutInflater layoutInflater = LayoutInflater.from(this);
-        View AddCourseView = layoutInflater.inflate(R.layout.dialog_addcourse, null);
-        final AlertDialog alertD = new AlertDialog.Builder(this)
-                .setTitle(R.string.add_course_dialog)
-                .create();
-        EditText courseInput = (EditText) AddCourseView.findViewById(R.id.course_name);
-        Button colorBtn = (Button) AddCourseView.findViewById(R.id.button_color);
-        Button saveBtn = (Button) AddCourseView.findViewById(R.id.button_addcourse_save);
-        Button cancelBtn = (Button) AddCourseView.findViewById(R.id.button_addcourse_cancel);
-        // OnClick Callbacks
-        final ColorPicker cp = new ColorPicker(this, 0,0,0);
-        cp.enableAutoClose();
-
-        alertD.setView(AddCourseView);
-        alertD.show();
-
-        // Show Course add dialog after color is picked
-        cp.setCallback(new ColorPickerCallback() {
-            @Override
-            public void onColorChosen(@ColorInt int color) {
-                alertD.show();
-                View colorSquare = alertD.findViewById(R.id.color_square);
-                colorSquare.setBackgroundColor(color);
-            }
-        });
-
-        // Show Color picker on color button press
-        colorBtn.setOnClickListener(v -> {
-            alertD.hide();
-            cp.show();
-        });
-
-        // Validate Course inputs and add to DB
-        saveBtn.setOnClickListener(v -> {
-            // Don't let user save if
-            //  - Course is empty
-            //  - Course already exists***
-            int color = Color.TRANSPARENT;
-            View colorSquare = alertD.findViewById(R.id.color_square);
-            Drawable background = colorSquare.getBackground();
-            if (background instanceof ColorDrawable)
-                color = ((ColorDrawable)background).getColor();
-
-            if (courseInput.getText().toString().matches("")) {
-                Toast.makeText(this, R.string.invalid_course, Toast.LENGTH_SHORT).show();
-            } else {
-                viewModel.insertCourse(new Course(
-                        courseInput.getText().toString(),
-                        new Date(),
-                        color));
-                alertD.dismiss();
-            }
-        });
-
-        // Quit on cancel press
-        cancelBtn.setOnClickListener(v -> alertD.dismiss());
+        mFab.setOnClickListener(v -> classSelectionFragment.showAddCourseDialog());
     }
 
     @Override
