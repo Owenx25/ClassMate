db2b25d1734e24c6f94a72d70e8db195a9409a9f
Jacob Phillips <jacob_phillips@student.uml.edu>
2018-11-26 08:05:52 -0500

    Revert "more ui changes some code added to DailyPageFragment"
    This reverts commit 2af2058b8cbfc1d93e0d00a9adec66e75367c118.

--- app/src/main/java/com/mobileapp/classmate/ui/DailyPageFragment.java
+++ app/src/main/java/com/mobileapp/classmate/ui/DailyPageFragment.java
@@ -1,48 +1,19 @@
 package com.mobileapp.classmate.ui;
 
-import android.arch.lifecycle.ViewModelProviders;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.os.Bundle;
-import android.widget.TextView;
 
 import com.mobileapp.classmate.R;
-import com.mobileapp.classmate.viewmodel.MainViewModel;
 
 public class DailyPageFragment extends Fragment {
-
-    private MainViewModel viewModel;
-
-    private TextView courseName;
-    private TextView courseColor;
-    private final AssignmentListAdapter adapter =
-            new AssignmentListAdapter(R.layout.daily_layout);
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(
-                R.layout.daily_layout, container, false);
-
-
-
-        RecyclerView recyclerView = (RecyclerView) rootView
-                .findViewById(R.id.assignment_selection_recycler);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-
-        // Setup ViewModel and Adapter
-        recyclerView.setAdapter(adapter);
-        /* Commented out because this will display assignments only from "chemistry" class
-           should be fixed to use getAllAssignments() once implemented */
-        //viewModel = ViewModelProviders.of(this).get(MainViewModel.class);
-        //viewModel.getCourseAssignments("chemistry").observe(this, assignments -> adapter.setAssignments(assignments));
-        return rootView;
+        return inflater.inflate(R.layout.daily_layout, container, false);
     }
-
 }
-
